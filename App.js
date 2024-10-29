f1718613088446f72eb4fc04e759519736f9f759
Duy2763 <tduy2706@gmail.com>
2024-10-29 15:05:26 +0700

    Update API

--- App.js
+++ App.js
@@ -6,79 +6,53 @@ import { ScrollView } from 'react-native';
 
 export default function App() {
   const [category, setCategory] = useState([
-    {id: '1', name: 'Resort', image: require('./assets/images/resort.png')},
-    {id: '2', name: 'Homestay', image: require('./assets/images/homestay.png')},
-    {id: '3', name: 'Hotel', image: require('./assets/images/hotel.png')},
-    {id: '4', name: 'Lodge', image: require('./assets/images/lodge.png')},
-    {id: '5', name: 'Villa', image: require('./assets/images/hostel.png')},
-    {id: '6', name: 'Apartment', image: require('./assets/images/apartment.png')},
-    {id: '7', name: 'Hostel', image: require('./assets/images/hostel.png')},
-    {id: '8', name: 'See all', image: require('./assets/images/seeall.png')},
+    // {id: '1', name: 'Resort', image: require('./assets/images/resort.png')},
+    // {id: '2', name: 'Homestay', image: require('./assets/images/homestay.png')},
+    // {id: '3', name: 'Hotel', image: require('./assets/images/hotel.png')},
+    // {id: '4', name: 'Lodge', image: require('./assets/images/lodge.png')},
+    // {id: '5', name: 'Villa', image: require('./assets/images/hostel.png')},
+    // {id: '6', name: 'Apartment', image: require('./assets/images/apartment.png')},
+    // {id: '7', name: 'Hostel', image: require('./assets/images/hostel.png')},
+    // {id: '8', name: 'See all', image: require('./assets/images/seeall.png')},
   
   ])
   const [location, setLocation] = useState([
-    {id: '1', image: require('./assets/images/photo1.png')},
-    {id: '2', image: require('./assets/images/photo2.png')},
-    {id: '3', image: require('./assets/images/photo3.png')},
-    {id: '4', image: require('./assets/images/photo4.png')},
-    {id: '5', image: require('./assets/images/photo5.png')},
+    // {id: '1', image: require('./assets/images/photo1.png')},
+    // {id: '2', image: require('./assets/images/photo2.png')},
+    // {id: '3', image: require('./assets/images/photo3.png')},
+    // {id: '4', image: require('./assets/images/photo4.png')},
+    // {id: '5', image: require('./assets/images/photo5.png')},
   ])
 
-  // const fetchLocation = async () => {
-  //   try {
-  //     const response = await axios.get('https://671b8cba2c842d92c3806828.mockapi.io/api/v1/locations');
-  //     setLocation(response.data);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
+  const fetchLocation = async () => {
+    try {
+      const response = await axios.get('https://671b8cba2c842d92c3806828.mockapi.io/api/v1/locations');
+      setLocation(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-  // const fetchCategory = async () => {
-  //   try {
-  //     const response = await axios.get('https://671b8cba2c842d92c3806828.mockapi.io/api/v1/categories');
-  //     setCategory(response.data);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
+  const fetchCategory = async () => {
+    try {
+      const response = await axios.get('https://671b8cba2c842d92c3806828.mockapi.io/api/v1/categories');
+      setCategory(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-  // const fetchLocation = async () => {
-  //   try {
-  //     const response = await fetch('https://671b8cba2c842d92c3806828.mockapi.io/api/v1/locations');
-  //     if (!response.ok) {
-  //       throw new Error(`HTTP error! status: ${response.status}`);
-  //     }
-  //     const data = await response.json(); // Chuyển đổi phản hồi sang JSON
-  //     setLocation(data); // Sử dụng dữ liệu đã chuyển đổi
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
-  
-  // const fetchCategory = async () => {
-  //   try {
-  //     const response = await fetch('https://671b8cba2c842d92c3806828.mockapi.io/api/v1/categories');
-  //     if (!response.ok) {
-  //       throw new Error(`HTTP error! status: ${response.status}`);
-  //     }
-  //     const data = await response.json(); // Chuyển đổi phản hồi sang JSON
-  //     setCategory(data); // Sử dụng dữ liệu đã chuyển đổi
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   fetchCategory();
-  //   fetchLocation();
-  // }, [])
+  useEffect(() => {
+    fetchCategory();
+    fetchLocation();
+  }, [])
 
   return (
     <SafeAreaView style={{flex: 1}}>
-      <View style={{backgroundColor: 'rgb(121, 120, 193)', padding: 24, gap: 24}}> 
+      <View style={styles.container}> 
         <View style={{gap: 16, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}> 
           <Image
-            style={{width: 40, height: 40}}
+            style={{width: 40, height: 40, borderRadius: '50%'}}
             source={require('./assets/images/logoicon.png')}
           />
           <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', backgroundColor: '#fff',  paddingVertical: 4, paddingHorizontal: 8, borderRadius: 8}}> 
@@ -105,16 +79,15 @@ export default function App() {
             </View>
           </View>
           <Image
-              style={{width: 20, height: 20}}
-              source={require('./assets/images/logoicon.png')}
+              style={{width: 35, height: 35, borderRadius: '50%'}}
+              source={require('./assets/images/ringicon.png')}
             />
         </View>
       </View>
-      
-      <ScrollView style={{flex: 1, paddingHorizontal: 48, paddingVertical: 16}}>
+      <ScrollView style={{flex: 1, paddingHorizontal: 32, paddingVertical: 16}}>
         <View style={{}}>
-          <View style={{paddingBottom: 16, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-              <Text>Category</Text>
+          <View style={styles.flatTop}>
+              <Text style={styles.flatTopTitle}>Category</Text>
               <Image
                 style={{width: 20, height: 20}}
                 source={require('./assets/images/3gach.png')}
@@ -126,9 +99,9 @@ export default function App() {
             renderItem={({item}) => (
               <View style={{alignItems: 'center',}}>
                   <Image
-                    style={{width: 60, height: 60}}
-                    // source={{ uri: item.image }}
-                    source={item.image}
+                    style={styles.flatImageCategory}
+                    source={{uri: item.image}}
+                    // source={item.image}
                   />
                   <Text>
                       {item.name}
@@ -143,8 +116,8 @@ export default function App() {
           
         </View>
         <View style={{paddingTop: 16}}>
-          <View style={{paddingBottom: 16, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-              <Text>Popular Destination</Text>
+          <View style={styles.flatTop}>
+              <Text style={styles.flatTopTitle}>Popular Destination</Text>
               <Image
                 style={{width: 20, height: 20}}
                 source={require('./assets/images/3gach.png')}
@@ -156,9 +129,9 @@ export default function App() {
             renderItem={({item}) => (
               <View style={{alignItems: 'center',}}>
                   <Image
-                    style={{width: 85, height: 85, borderRadius: 8}}
-                    // source={{ uri: item.image }}
-                    source={item.image}
+                    style={styles.flatImagePopular}
+                    source={{uri: item.image}}
+                    // source={item.image}
                   />
                   <Text>
                       {item.name}
@@ -173,8 +146,8 @@ export default function App() {
           
         </View>
         <View style={{paddingTop: 16}}>
-          <View style={{paddingBottom: 16, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-              <Text>Recommended</Text>
+          <View style={styles.flatTop}>
+              <Text style={styles.flatTopTitle}>Recommended</Text>
           </View>
           <FlatList
             data={location.filter(item => item.id > 3)}
@@ -182,9 +155,9 @@ export default function App() {
             renderItem={({item}) => (
               <View style={{alignItems: 'center',}}>
                   <Image
-                    style={{width: 140, height: 85, borderRadius: 8}}
-                    // source={{ uri: item.image }}
-                    source={item.image}
+                    style={styles.flatImageRecommend}
+                    source={{uri: item.image}}
+                    // source={item.image}
                   />
                   <Text>
                       {item.name}
@@ -201,30 +174,30 @@ export default function App() {
       </ScrollView>
 
       <View style={{backgroundColor: 'rgb(121, 120, 193)', flexDirection: 'row', justifyContent: 'space-evenly', paddingTop: 16, paddingBottom: 24}}> 
-        <View>
+        <View style={styles.footerItem}>
           <Image
-            style={{width: 30, height: 30}}
+            style={styles.footerImage}
             source={require('./assets/images/homeicon.png')}
           />
           <Text style={{color: '#fff'}}>Home</Text>
         </View>
-        <View>
+        <View style={styles.footerItem}>
           <Image
-            style={{width: 30, height: 30}}
+            style={styles.footerImage}
             source={require('./assets/images/exploreicon.png')}
           />
           <Text style={{color: '#fff'}}>Explore</Text>
         </View>
-        <View>
+        <View style={styles.footerItem}>
           <Image
-            style={{width: 30, height: 30}}
+            style={styles.footerImage}
             source={require('./assets/images/searchicon.png')}
           />
           <Text style={{color: '#fff'}}>Search</Text>
         </View>
-        <View>
+        <View style={styles.footerItem}>
           <Image
-            style={{width: 30, height: 30}}
+            style={styles.footerImage}
             source={require('./assets/images/profileicon.png')}
           />
           <Text style={{color: '#fff'}}>Profile</Text>
@@ -237,6 +210,40 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    
+    backgroundColor: 'rgb(121, 120, 193)', 
+    padding: 24, 
+    gap: 24
   },
+  flatTop: {
+    paddingBottom: 16, 
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    alignItems: 'center'
+  },
+  flatTopTitle: {
+    fontWeight: 'bold'
+  },
+  flatImageCategory: {
+    width: 60, 
+    height: 60
+  },
+  flatImagePopular: {
+    width: 100,
+    height: 100, 
+    borderRadius: 8
+  },
+  flatImageRecommend: {
+    width: 157, 
+    height: 85, 
+    borderRadius: 8
+  },
+  footerImage: {
+    width: 40,
+    height: 40,
+  },
+  footerItem: {
+    alignItems: 'center'
+  }
+
+  
 });
